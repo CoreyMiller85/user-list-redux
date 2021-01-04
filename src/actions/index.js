b02ae9476bc61356652df3b5c71d01069b2ad541
Corey Miller <coreymiller454@gmail.com>
2021-01-04 15:31:37 -0500

    corrects issue with over fetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+// import _ from "lodash";
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -9,3 +10,32 @@ export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+/*
+
+call function with id fetchUser(2) that will return 
+
+
+const fetchUser = function(id) {
+  async function(dispatch) {
+    const response = await jsonPlaceholder.get('users/3');
+    dispatch({type: "FETCH_USER", payload: response.data})
+  }
+}
+
+*/
+
+/* 
+Memoize way of solving over-fetching isse 
+*/
+
+// Function that returns a function that calls a function.
+
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
