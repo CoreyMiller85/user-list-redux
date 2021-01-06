5ace877ac542f4f9fe24b4dc687ea6e9774d73f7
Corey Miller <coreymiller454@gmail.com>
2021-01-05 23:36:01 -0500

    adds comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,8 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   // const userIds = _.uniq(_.map(getState().posts, "userId"));
   // userIds.forEach((id) => dispatch(fetchUser(id)));
 
+  //this is lodash chain method, called in left to right, calls chain, gets posts from state, maps the userID key, filters all uniques out, fetches user info for each unique id
+  // and then .value calls all functions.
   _.chain(getState().posts)
     .map("userId")
     .uniq()
